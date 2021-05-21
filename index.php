3738843c0cee37b23668a6db5c84615cc0b29371
Musab <musabwebdev@gmail.com>
2021-05-21 19:48:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
 
-$qr_code = "";
-
-// Check $_POST Super Global Variable isset or not
 if (isset($_POST["submit"])) {
     $data = $_POST["data"];
-    $width = $_POST["width"];
-    $height = $_POST["height"];
+    if ($_POST["width"] != "") {
+        $width = $_POST["width"];
+    } else {
+        $width = "250";
+    }
+    if ($_POST["height"] != "") {
+        $height = $_POST["height"];
+    } else {
+        $height = "250";
+    }
 
-    // Hit this url for get QR Code
     $url = "https://chart.googleapis.com/chart?cht=qr&chs={$width}x{$height}&chl={$data}";
-
-    $qr_code = '<img style="max-width:100%;max-height:100%;" width="'.$width.'" height="'.$height.'" src="'.$url.'">';
+    $output["img"] = $url;
 }
 
 ?>
@@ -24,34 +27,42 @@ if (isset($_POST["submit"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>QR Code Generator</title>
+
+    <title>QR Code Generator - Pure Coding</title>
 </head>
 <body class="bg-light">
     <div class="container py-5">
         <div class="row">
-            <div class="mx-auto col-lg-5 bg-white p-3 shadow rounded">
-                <h1 class="text-center mb-3 fw-bold">QR Code Generator</h1>
-                <div class="my-3"><?php echo $qr_code; ?></div>
-                <form method="post">
-                    <div class="mb-3">
+            <div class="col-md-5 col-12 shadow bg-white border mx-auto p-4">
+                <h2 class="text-center fw-bold mb-3">QR Code Generator</h2>
+                <?php if (isset($output)) { ?>
+                <div class="mb-3">
+                    <img src="<?php echo $output["img"] ?>" alt="QR Code" width="100%" height="100%">
+                </div>
+                <?php } ?>
+                <form action="" method="post">
+                    <div class="form-group mb-3">
                         <label for="data" class="form-label">Data</label>
-                        <input type="text" class="form-control" name="data" placeholder="e.g. Hello World" id="data" required>
+                        <input type="text" class="form-control" id="data" name="data" placeholder="Ex: Hello World" required>
                     </div>
-                    <div class="row mb-3">
-                        <label class="form-label">Size</label>
+                    <div class="form-group row">
                         <div class="col-6">
-                            <input type="number" class="form-control" name="width" placeholder="Width e.g. 250" required>
+                            <label for="width" class="form-label">Width</label>
+                            <input type="number" class="form-control" id="width" name="width" placeholder="Ex: 250px">
                         </div>
                         <div class="col-6">
-                            <input type="number" class="form-control" name="height" placeholder="Height e.g. 250" required>
+                            <label for="height" class="form-label">Height</label>
+                            <input type="number" class="form-control" id="height" name="height" placeholder="Ex: 250px">
                         </div>
                     </div>
-                    <button type="submit" name="submit" class="btn btn-primary">Get QR Code</button>
+                    <div class="mt-3">
+                        <button type="submit" name="submit" class="btn btn-primary">Generate QR Code!</button>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
-
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
